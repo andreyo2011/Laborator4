195e100865d31177478ae745088e0d82a956b4a9
andreyo2011 <andreyo_97@yahoo.com>
2022-09-04 17:25:53 +0200

    Add files via upload

--- Laborator3-main4/Laborator3-main/Laborator3/Laborator3/SearchArticle/SearchArticleViewController.swift
+++ Laborator3-main4/Laborator3-main/Laborator3/Laborator3/SearchArticle/SearchArticleViewController.swift
@@ -22,26 +22,55 @@ final class SearchArticleViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
+        //getAllModels()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         getAllModels()
     }
     
     private func configure(){
-        searchTextField?.delegate = self
+        searchTextField?.addTarget(self, action: #selector(textFieldDdidChange), for: .editingChanged)
+        //searchTextField?.delegate = self
         tableView?.delegate = self
         tableView?.dataSource = self
         tableView?.register(SearchArticleTableViewCell.self, forCellReuseIdentifier: SearchArticleTableViewCell.cellId)
         
     }
     
-    private func getAllModels(){
+    @objc
+    private func textFieldDdidChange(){
+        let searchString = searchTextField?.text ?? String()
+        getAllModels(searchString: searchString)
+        
+    }
+    
+    private func navigate(item: Item){
+        guard let viewController = storyboard?.instantiateViewController(withIdentifier: "ArticleViewController")
+        as? ArticleViewController else {return}
+        viewController.item = item //inlocuiteste cele 2 de jos in urma modificarilor ArticleViewController
+        //viewController.textView?.text = item.body
+        //viewController.title = item.title
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    private func getAllModels(searchString: String = String()){
         
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         let managedContext = appDelegate.persistentContainer.viewContext
         
         let fetchRequest = RatingEntity.fetchRequest()
         //fetchRequest.predicate() sortari,filtrari etc
+        if !searchString.isEmpty{
+            fetchRequest.predicate = NSPredicate(format: "body CONTAINS %@", searchString)
+        }
         do{
-            model = try managedContext.fetch(fetchRequest)
+            model = try managedContext.fetch(fetchRequest)/*.filter{ entity in
+                return entity.body?.contains(searchString) ?? false
+                
+                
+            }*/
             
         }catch{
             print("here::",error.localizedDescription)
@@ -68,6 +97,17 @@ extension SearchArticleViewController: UITableViewDelegate, UITableViewDataSourc
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return model.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = model[indexPath.row]
+        let item = Item(title: model.name ?? String(),
+                        date: "",
+                        body: model.body ?? String()
+                        )
+        navigate(item: item)
+        
     }
     
 }
